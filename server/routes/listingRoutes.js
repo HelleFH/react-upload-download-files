efe9ff7ce737f942965330e49b2d34d900350497
Your Name <you@example.com>
2024-03-09 00:56:37 +0100

    .

--- server/routes/listingRoutes.js
+++ server/routes/listingRoutes.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {
-  deleteImage,
-  deleteListing,
-  getListingById,
-  getListings,
-  updateListing,
-  uploadListing,
-} = require('../controller/listingController');
-const upload = require('../middleware/upload');
 const cloudinary = require('cloudinary').v2;
+const { Listing } = require('../model/listingModel');
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -17,11 +9,100 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-router.get('/:id', getListingById);
-router.get('/', getListings);
-router.put('/:id', updateListing);
-router.delete('/:id/delete-image', deleteImage);
-router.delete('/:id', deleteListing);
-router.post('/upload', upload.single('file'), uploadListing);
+router.get('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const listing = await Listing.findById(id);
+
+    if (!listing) {
+      return res.status(404).json({ error: 'Listing not found' });
+    }
+
+    res.status(200).json(listing);
+  } catch (error) {
+    console.error('Error fetching listing:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const listings = await Listing.find();
+    res.json(listings);
+  } catch (error) {
+    console.error('Error fetching listings:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  const id = req.params.id;
+  const { title, description, location, cloudinaryUrl } = req.body;
+  const updatedData = req.body;
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(id, updatedData, { new: true });
+    res.json(updatedListing);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.delete('/:id/delete-image', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const listing = await Listing.findById(id);
+
+    if (!listing) {
+      return res.status(404).json({ error: 'Listing not found' });
+    }
+
+    await cloudinary.uploader.destroy(listing.cloudinaryUrl);
+
+    res.json({ msg: 'Image deleted successfully.' });
+  } catch (error) {
+    console.error('Error deleting image:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedListing = await Listing.findByIdAndDelete(id);
+
+    if (!deletedListing) {
+      return res.status(404).json({ error: 'Listing not found' });
+    }
+
+    res.json({ message: 'Listing deleted successfully', deletedListing });
+  } catch (error) {
+    console.error('Error deleting listing:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.post('/upload', async (req, res) => {
+  try {
+    const { path } = req.file;
+    const { title, description, location } = req.body;
+
+    const result = await cloudinary.uploader.upload(path);
+
+    const listing = new Listing({
+      title,
+      description,
+      location,
+      cloudinaryUrl: result.secure_url,
+    });
+
+    await listing.save();
+    res.json({ msg: 'Listing data uploaded successfully.' });
+  } catch (error) {
+    console.error('Error while uploading listing data:', error);
+    res.status(400).json({ error: 'Error while uploading listing data. Try again later.' });
+  }
+});
 
 module.exports = router;
