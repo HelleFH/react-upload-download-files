09a642c43b01376c638f9d1eefb9f783731c8917
Your Name <you@example.com>
2024-03-09 03:55:02 +0100

    .

--- src/pages/createListing.jsx
+++ src/pages/createListing.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { API_URL } from '../utils/constants';
 import ImageUpload from '../components/imageUpload';
 import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'; 
 
 
 const CreateListingWithFileUpload = () => {
