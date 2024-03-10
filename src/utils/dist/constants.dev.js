4f18a6a984849615ffa0ae2a326a85bfb44bc614
Your Name <you@example.com>
2024-03-10 10:00:37 +0100

    .

--- src/utils/dist/constants.dev.js
+++ src/utils/dist/constants.dev.js
@@ -4,5 +4,5 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.API_URL = void 0;
-var API_URL = 'http://localhost:3030';
+var API_URL = 'https://react-upload-download-files-be.onrender.com';
 exports.API_URL = API_URL;
